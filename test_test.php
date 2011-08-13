029f9873ab00b1ef69949ded8602997042f2fd41
Nikolay Bachiyski <nb@nikolay.bg>
2011-08-13 15:39:20 -0700

    Change the sample test to is_mail()-only test

--- test_test.php
+++ test_test.php
@@ -1,22 +1,21 @@
 <?php
 require_once dirname( __FILE__ ) . '/init.php';
 
-class WP_Test_Test extends WP_UnitTestCase {
+class WP_Is_Email_Test extends WP_UnitTestCase {
 
-	function test_is_email() {
-		$this->assertEquals( 'nb@nikolay.bg', is_email( 'nb@nikolay.bg' ) );
+	function test_is_email_only_letters_with_dot_com_domain() {
+		$this->assertEquals( 'nb@nikolay.com', is_email( 'nb@nikolay.com' ) );
+	}
+	
+	function test_is_email_should_not_allow_missing_tld() {
 		$this->assertFalse( is_email( 'nb@nikolay' ) );
 	}
 	
-	function test_option() {
-		$this->assertEquals( false, get_option( 'baba' ) );
-		add_option( 'baba', 'dyado' );
-		$this->assertEquals( 'dyado', get_option( 'baba' ) );
+	function test_is_email_should_allow_bg_domain() {
+		$this->assertEquals( 'nb@nikolay.bg', is_email( 'nb@nikolay.bg' ) );
 	}
-	
-	function test_array_option() {
-		$this->assertEquals( false, get_option( 'baba' ) );
-		add_option( 'baba', array( 'baba' => 'dyado' ) );
-		$this->assertEquals( array( 'baba' => 'dyado' ), get_option( 'baba' ) );
-	}	
+
+	function test_is_email_should_not_allow_blah_domain() {
+		$this->assertFalse( is_email( 'nb@nikolay.blah' ) );
+	}
 }
